a69eb9885e0dc9e27f4816ef87b137f15b51a7d2
Charles-04 <charlesmir04@gmail.com>
2023-04-21 10:47:46 +0100

    feat: Added DbContext

---
+++ Poemify.DAL/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Poemify.DAL.Enums;
+
+
+namespace Poemify.DAL.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public UserType UserType { get; set; } = UserType.Reader;
+        public string Image { get; set; }
+    }
+}
---
+++ Poemify.DAL/Entities/Poem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poemify.DAL.Entities
+{
+    public class Poem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Type { get; set; }
+        public  { }
+
+    }
+}
