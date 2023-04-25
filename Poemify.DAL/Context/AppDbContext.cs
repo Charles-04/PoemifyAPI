966f516649347c7685628dd90cfc09a77fe833cb
Charles-04 <charlesmir04@gmail.com>
2023-04-25 15:14:08 +0100

    Fix : For Tag name not unique

--- Poemify.DAL/Context/AppDbContext.cs
+++ Poemify.DAL/Context/AppDbContext.cs
@@ -9,12 +9,17 @@ namespace Poemify.DAL.Context
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+        DbSet<AppUser> Users { get; set; }
+        DbSet<Poem> Poems { get; set; }
+        DbSet<Tag> Tags { get; set; }
+        DbSet<Comment> Comments { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Tag>().HasIndex(t => t.Name).IsUnique();
+            modelBuilder.Entity<AppUser>().HasIndex(u => u.UserName).IsUnique();
 
         }
-        DbSet<AppUser> Users { get; set; }
-        DbSet<Poem> Poems { get; set; }
+       
     }
 }
