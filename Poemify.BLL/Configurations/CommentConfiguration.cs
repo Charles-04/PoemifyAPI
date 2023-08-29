d3fa5106c668102d3b8d73d8babeb8fe0fef1368
Charles-04 <charlesmir04@gmail.com>
2023-08-29 15:52:18 +0100

    Created mapping cnfigurations for comments

---
+++ Poemify.BLL/Configurations/CommentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Poemify.Models.DTOs.Response;
+using Poemify.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poemify.BLL.Configurations
+{
+    public class CommentConfiguration : Profile
+    {
+        public CommentConfiguration() {
+            CreateMap<Comment, CommentResponse>()
+                   .ForMember(dest => dest.Comment, opt => opt.MapFrom(src => src.Content))
+                   .ForMember(dest => dest.PoemTitle, opt => opt.MapFrom(src => src.Poem.Title))
+                   .ForMember(dest => dest.Commenter, opt => opt.MapFrom(src => src.Author.UserName));
+        }
+    }
+}
